261e324519e560e0401fe7c71ba15c8f12be6793
Rohan27s <rohangotwal3@gmail.com>
2023-12-09 04:58:43 +0000

    a

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // server.js
 import express from 'express'
 const app = express();
-const baseUrl = "https://backend-rohan27s.vercel.app";
+// const baseUrl = "https://backend-rohan27s.vercel.app";
 import auth from './routes/auth.js'
 import connectDb from './middleware/mongoose.js';
 import * as dotenv from 'dotenv' 
 dotenv.config()
+const port = process.env.PORT || 5000;
 
 // Define a route
-app.set('baseUrl', baseUrl);
+// app.set('baseUrl', baseUrl);
 
 const DATABASE_URL = process.env.DATABASE_URL;
-console.log(DATABASE_URL,'url')
+// console.log(DATABASE_URL,'url')
 app.use(express.json());
 connectDb("mongodb+srv://rohangotwal:rohan123@cluster0.nvvzqtz.mongodb.net/?retryWrites=true&w=majority");
 app.get('/', (req, res) => {
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 });
 app.use("/api/auth",auth);
 // Start the server
-const port = 3000
+// const port = 3000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
